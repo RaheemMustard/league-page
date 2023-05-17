3d14ef8b11ab8f667710d722b9a4df4da083bfa9
RaheemMustard <133824691+RaheemMustard@users.noreply.github.com>
2023-05-16 19:12:58 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,12 +7,14 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>Weclome to the NFC League! This league is part of a greater league called the DFL, and consists of one other league, the AFC.</p>
+  <p>This league started in 2021 and is run and commissioned by Magma. There are a total of 12 teams.</p>
+  <p>The DFL as a whole is run and commissioned by Mars.</p>
+  <p>In the 2 years the DFL has been running, the NFC has won one SB, being the first SB of the DFL. It was was won by Magma.</p>
+  <p>There are 3 divisions, the NFC West, NFC Central, and NFC East. The teams that represent these divisions and the NFC are:</p>
+  <p>NFC West: Bags, Jp, Lame, Hark</p>
+  <p>NFC Central: Magma, Maverick, Dagger, Werdna</p>
+  <p>NFC East: Juhana, Monsta, Moose, Madness</p>
 `;
 
 /*   STEP 3   */
